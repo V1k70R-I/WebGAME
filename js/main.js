b9eca26c18d04c537a204b1989c228fe22342cd5
viktor.shslyapin <v.shalyapin3323@gmail.com>
2021-04-12 17:09:59 +0300

    drawing elements falling down

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 window.onload = () => {
-  let tetris = [];
-  let tetrisField = document.querySelector("#tetris-field");
-  let scoreField = document.querySelector(".score-field");
   let timer;
-  let score = 0;
 
-  // Заполняем массив
+  // Заполняем массив (псевдополе)
+
+  let tetris = [];
 
   function init() {
     let column = 9;
@@ -20,26 +18,25 @@ window.onload = () => {
 
   // Рисуем игровое поле
 
+  let tetrisField = document.querySelector("#tetris-field");
+  let scoreField = document.querySelector(".score-field");
+  let score = 0;
+
   function draw() {
     let out = "";
     for (let i = 0; i < tetris.length; i++) {
       for (let j = 0; j < tetris[i].length; j++) {
         if (tetris[i][j] == 0) {
           out += '<div class="circle white"></div>';
-        }
-        else if (tetris[i][j] == 1) {
+        } else if (tetris[i][j] == 1 || tetris[i][j] == 11) {
           out += '<div class="circle red"></div>';
-        }
-        else if (tetris[i][j] == 2) {
+        } else if (tetris[i][j] == 2 || tetris[i][j] == 12) {
           out += '<div class="circle gold"></div>';
-        }
-        else if (tetris[i][j] == 3) {
+        } else if (tetris[i][j] == 3 || tetris[i][j] == 13) {
           out += '<div class="circle green"></div>';
-        }
-        else if (tetris[i][j] == 4) {
+        } else if (tetris[i][j] == 4 || tetris[i][j] == 14) {
           out += '<div class="circle blue"></div>';
-        }
-        else if (tetris[i][j] == 5) {
+        } else if (tetris[i][j] == 5 || tetris[i][j] == 15) {
           out += '<div class="circle pink"></div>';
         }
       }
@@ -58,10 +55,38 @@ window.onload = () => {
     tetris[0][0] = randomInteger(1, 5);
   }
 
+  // Запуск игры ("Start")
+
+  let buttonRun = document.querySelector("#start");
+  let flag; // Проверка когда запускать следующий элемент
+
+  function start() {
+    draw();
+    flag = true;
+    for (let i = tetris.length - 1; i >= 0; i--) {
+      for (let j = 0; j < tetris[i].length; j++) {
+        if (tetris[i][j] < 9) {
+          if (tetris[i][j] != 0) {
+            if (i == tetris.length - 1) {
+              tetris[i][j] = tetris[i][j] + 10;
+            } else if (tetris[i + 1][j] == 0) {
+              tetris[i + 1][j] = tetris[i][j];
+              tetris[i][j] = 0;
+              flag = false;
+            }
+          }
+        }
+      }
+    }
+    if (flag) {
+      circle();
+    }
+  }
+
   // Запускаем
 
   init();
   draw();
   circle();
-  draw();
+  buttonRun.onclick = start;
 };
